a8a081fa23900dd3347c2bacaae08f158f0cc2b2
OxyTom <oxytom8@gmail.com>
2019-10-13 03:50:46 +0200

    initial commit

---
+++ icalToHtmlToJpg/Event.cs
@@ -0,0 +1,174 @@
+﻿using System;
+
+namespace icalToHtmlToJpg
+{
+    class Event
+    {
+        #region Private Fields
+
+        ///  <summary>
+        /// The name of the event
+        ///  </summary>
+        private string name = "";
+
+        ///  <summary>
+        /// The location of the event
+        ///  </summary>
+        private string loc = "";
+
+        ///  <summary>
+        /// The start time of the event
+        ///  </summary>
+        private string start = "";
+
+        ///  <summary>
+        /// The end time of the event
+        ///  </summary>
+        private string end = "";
+
+        ///  <summary>
+        /// The day of the event
+        ///  </summary>
+        private int day = 1;
+
+        ///  <summary>
+        /// The month of the event
+        ///  </summary>
+        private int month = 1;
+
+        #endregion
+
+        #region Public Methods
+
+        ///  <summary>
+        ///  Create an event with a name, a start time, a end time and a localisation.
+        ///  </summary>
+        ///  <param name="name">The name of the event.</param>
+        ///  <param name="start">The start time of the event.</param>
+        ///  <param name="end">The end time of the event.</param>
+        ///  <param name="loc">The localisation of the event.</param>
+        ///  <remarks>
+        ///  Raw input only !
+        ///  </remarks>
+        public void CreateEvent(string name, string start, string end, string loc)
+        {
+            this.name = name;
+            this.loc = loc;
+            this.start = start;
+            this.end = end;
+        }
+
+
+        ///  <summary>
+        ///  Clean variables to be ready to extract.
+        ///  Also create the day and the month.
+        ///  </summary>
+        public void CleanEvent()
+        {
+            /*== Clean the name ==*/
+            string[] splittedName = new string[5];
+            string[] separator = { ":", " - " };
+            splittedName = this.name.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            this.name = splittedName[1];
+
+            /*== Clean the localisation ==*/
+            string[] splitted = new string[2];
+            splitted = this.loc.Split(':', StringSplitOptions.RemoveEmptyEntries);
+            this.loc = splitted[1];
+
+            /*== Clean the start time ==*/
+            splitted = this.start.Split(':', StringSplitOptions.RemoveEmptyEntries);
+            splitted = splitted[1].Split('T', StringSplitOptions.RemoveEmptyEntries);
+            char[] sHours = { splitted[1][0], splitted[1][1] };
+            char[] sMins = { splitted[1][2], splitted[1][3] };
+            this.start = new string(sHours) + ":" + new string(sMins);
+
+            /*== Create date day ==*/
+            char[] dayChar = { splitted[0][6], splitted[0][7] };
+            this.day = int.Parse(new string(dayChar));
+
+            /*== Create date month ==*/
+            char[] monthChar = { splitted[0][4], splitted[0][5] };
+            this.month = int.Parse(new string(monthChar));
+
+            /*== Clean end time ==*/
+            splitted = this.end.Split(':', StringSplitOptions.RemoveEmptyEntries);
+            splitted = splitted[1].Split('T', StringSplitOptions.RemoveEmptyEntries);
+            char[] eHours = { splitted[1][0], splitted[1][1] };
+            char[] eMins = { splitted[1][2], splitted[1][3] };
+            this.end = new string(eHours) + ":" + new string(eMins);
+        }
+
+        #endregion
+
+        #region Gets Fields
+
+        ///  <summary>
+        ///  Return the name
+        ///  </summary>
+        ///  <returns>
+        ///  Name of the event
+        ///  </returns>
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        ///  <summary>
+        ///  Return the localisation
+        ///  </summary>
+        ///  <returns>
+        ///  localisation of the event
+        ///  </returns>
+        public string GetLoc()
+        {
+            return this.loc;
+        }
+
+        ///  <summary>
+        ///  Return the start time
+        ///  </summary>
+        ///  <returns>
+        ///  Start time of the event
+        ///  </returns>
+        public string GetStart()
+        {
+            return this.start;
+        }
+
+        ///  <summary>
+        ///  Return the end time
+        ///  </summary>
+        ///  <returns>
+        ///  End time of the event
+        ///  </returns>
+        public string GetEnd()
+        {
+            return this.end;
+        }
+
+        ///  <summary>
+        ///  Return the day
+        ///  </summary>
+        ///  <returns>
+        ///  Day of the event
+        ///  </returns>
+        public int GetDay()
+        {
+            return this.day;
+        }
+
+        ///  <summary>
+        ///  Return the month
+        ///  </summary>
+        ///  <returns>
+        ///  Month of the event
+        ///  </returns>
+        public int GetMonth()
+        {
+            return this.month;
+        }
+
+        #endregion
+    }
+}
---
+++ icalToHtmlToJpg/ICalToJpg.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using CoreHtmlToImage;
+
+// TODO: Colorier le jour en cours
+// TODO 01 : Changement de mois
+// TODO 02 : Séparer en fonctions
+
+namespace icalToHtmlToJpg
+{
+    class ICalToJpg
+    {
+        #region Private Fields
+
+        private static string[] CalendarUrl { get; set; }
+
+        #endregion
+
+        #region Private Methods
+
+        static void Main(string[] args)
+        {
+            /*== Get Calendar ==*/
+            CalendarUrl = new string[8];
+            CalendarUrl[0] = "https://dptinfo.iutmetz.univ-lorraine.fr/lna/agendas/ical.php?ical=e81e5e310001831"; //1.1
+            CalendarUrl[1] = "https://dptinfo.iutmetz.univ-lorraine.fr/lna/agendas/ical.php?ical=4352c5485001785"; //1.2
+            CalendarUrl[2] = "https://dptinfo.iutmetz.univ-lorraine.fr/lna/agendas/ical.php?ical=329314450001800"; //2.1
+            CalendarUrl[3] = ""; //2.2
+            CalendarUrl[4] = ""; //3.1
+            CalendarUrl[5] = "https://dptinfo.iutmetz.univ-lorraine.fr/lna/agendas/ical.php?ical=b4a52df5e501843"; //3.2
+            CalendarUrl[6] = "https://dptinfo.iutmetz.univ-lorraine.fr/lna/agendas/ical.php?ical=561e49e97901779"; //4.1
+            CalendarUrl[7] = "https://dptinfo.iutmetz.univ-lorraine.fr/lna/agendas/ical.php?ical=3a1ee9527101771"; //4.2
+
+            var stringICal = new string[CalendarUrl.Length];
+            for (int i = 0; i < CalendarUrl.Length; i++)
+            {
+                try
+                {
+                    //we download the differents icals
+                    using (var wc = new WebClient())
+                        stringICal[i] = wc.DownloadString(CalendarUrl[i]);
+                }
+                catch (ArgumentException)
+                {
+                    //if no ical is provided, we just ignore it. I already give enough to the promotion, if they don't give me the link, fuck them.
+                    continue;
+                }
+            }
+
+            /*== Create Events ==*/
+            List<string[]> listEvents = new List<string[]>();
+            List<Event> events = new List<Event>();
+
+            DayOfWeek day = DateTime.Now.DayOfWeek;
+            int days = day - DayOfWeek.Monday;
+            DateTime start = DateTime.Now.AddDays(-days);
+            DateTime end = start.AddDays(6);
+
+            char[] startChar = { start.ToString()[0], start.ToString()[1] };
+            char[] endChar = { end.ToString()[0], end.ToString()[1] };
+
+            int idLine = 0;
+            int dateStart = int.Parse(new string(startChar));
+            int dateEnd = int.Parse(new string(endChar));
+            int month = DateTime.Now.Month;
+
+            bool isEndOfCal = false;
+
+            // Split the ICal line by line and add it in a List<>
+            listEvents.Add(stringICal[1].Split("\n", StringSplitOptions.RemoveEmptyEntries));
+
+            while (!isEndOfCal)
+            {
+                for (int j = 0; j < listEvents[idLine].Length; j++)
+                {
+                    // if it's a new event
+                    if (listEvents[idLine][j] == "BEGIN:VEVENT")
+                    {
+                        // create an event
+                        var ev = new Event();
+                        ev.CreateEvent(listEvents[idLine][j + 1], //name
+                                       listEvents[idLine][j + 2], //start date
+                                       listEvents[idLine][j + 3], //end date
+                                       listEvents[idLine][j + 4]); //localisation
+                        ev.CleanEvent();
+
+                        // if the event is in this week add it in another list
+                        int evDate = ev.GetDay();
+                        if (dateEnd >= evDate && evDate >= dateStart && ev.GetMonth() == month)
+                        {
+                            events.Add(ev);
+                        }
+
+                    }
+                    //if it's the end of the ICal, exit the loop
+                    else if (listEvents[idLine][j] == "END:VCALENDAR") { isEndOfCal = true; }
+                }
+                idLine++; // New line
+            }
+
+            /*== Create HTML ==*/
+
+            var body = new StringBuilder();
+            var finalHtml = new StringBuilder();
+
+            // Get a list of the dates in the week
+            List<int> dates = new List<int>();
+            IEnumerable<int> numbers = Enumerable.Range(dateStart, dateEnd - dateStart);
+            foreach (int n in numbers)
+            {
+                dates.Add(n);
+            }
+
+            string[] hours = { "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18" };
+            string[] minutes = { "00", "15", "30", "45" };
+
+            int id_h = 0;
+            int id_m = 0;
+            int[] delays = new int[6];
+
+            // For each line (there's 40 lines)
+            for (int i = 0; i < 40; i++)
+            {
+                int dcount = -1;
+
+                // Start of line
+                body.AppendLine("<tr>");
+
+                // Each 4 lines add the time
+                if (i % 4 == 0)
+                {
+                    body.AppendLine(string.Format(@"<th rowspan=""4"">{0}h-{1}h", hours[id_h], hours[id_h + 1]));
+                    id_h++;
+                }
+
+                // Each 4 lines, reset the index for minutes
+                if (id_m == 4)
+                {
+                    id_m = 1;
+                }
+                else
+                {
+                    id_m++;
+                }
+
+                // For each day
+                foreach (var d in dates)
+                {
+                    dcount++; // ID of the day
+                    if (delays[dcount] > 0) { delays[dcount]--; } // Reduce cooldown if exists
+                    bool isFound = false;
+
+                    // Check for each event if the time and the date matches
+                    foreach (var ev in events)
+                    {
+                        if (ev.GetStart() == string.Format("{0}:{1}", hours[id_h - 1], minutes[id_m - 1]) && ev.GetDay() == d)
+                        {
+                            // Translate complete str time to an int with hours only
+                            char[] evStartHChar = { ev.GetStart()[0], ev.GetStart()[1] };
+                            int evStartHInt = int.Parse(new string(evStartHChar));
+                            char[] evEndHChar = { ev.GetEnd()[0], ev.GetEnd()[1] };
+                            int evEndHInt = int.Parse(new string(evEndHChar));
+
+                            // Translate complete str time to an int with minutes only
+                            char[] evStartMChar = { ev.GetStart()[3], ev.GetStart()[4] };
+                            int evStartMInt = int.Parse(new string(evStartMChar));
+                            char[] evEndMChar = { ev.GetEnd()[3], ev.GetEnd()[4] };
+                            int evEndMInt = int.Parse(new string(evEndMChar));
+
+                            // Calc duration and add it to the HTML
+                            int delay = (evEndHInt - evStartHInt) * 4 + (evEndMInt - evStartMInt) / 15;
+                            body.AppendLine(string.Format(@"<td rowspan=""{0}"">{1} <br /> <i>{2}</i></td>", delay, ev.GetName(), ev.GetLoc()));
+
+                            // Remove the event
+                            events.Remove(ev);
+
+                            // Insert a cooldown to this day, the cd is the duration of the event
+                            isFound = true;
+                            delays[dcount] = delay;
+                            break;
+                        }
+                    }
+                    // If nothing was found, insert a blank cell
+                    if (!isFound && delays[dcount] == 0) { body.AppendLine("<td></td>"); }
+                }
+
+                // End of line
+                body.AppendLine("</tr>");
+            }
+
+            // Merge the pre-generated head and foot with the body previously generated
+            string head, foot;
+            using (var wc = new WebClient())
+                head = string.Format(wc.DownloadString("google.fr"), dateStart, dateEnd, month);
+            using (var wc = new WebClient())
+                foot = wc.DownloadString("google.fr");
+            finalHtml.Append(head + body + foot);
+
+            /*== Create JPG/HTML File ==*/
+            try
+            {
+                // jpeg file
+                var converter = new HtmlConverter();
+                var bytes = converter.FromHtmlString(finalHtml.ToString());
+                File.WriteAllBytes("edt12.jpg", bytes);
+            }
+            catch (Exception)
+            {
+                // if started on the C: drive (beacause stfu) then create an html file
+                using (FileStream fs = new FileStream("edt12.html", FileMode.Create))
+                {
+                    using (StreamWriter w = new StreamWriter(fs, Encoding.UTF8))
+                    {
+                        w.Write(finalHtml);
+                    }
+                }
+            }
+
+        }
+
+        #endregion
+    }
+}
